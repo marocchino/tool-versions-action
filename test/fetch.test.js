1973dfd130c90f0a67fd309fcd8248ba390ef08a
marocchino <marocchino@users.noreply.github.com>
2024-09-05 15:27:38 +0900

    ✅ test for hugo

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -3,6 +3,7 @@ const fetch = require("../fetch");
 test("load .tool-versions file to object", async () => {
   expect(fetch("./test/.tool-versions")).toStrictEqual({
     elixir: "1.9.4",
-    nodejs: "16.0.0"
+    hugo: '{"version":"0.85.0","is_extended":true}',
+    nodejs: "16.0.0",
   });
 });
